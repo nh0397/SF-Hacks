160abad26a97f07e8105a3e50f60a0dd8ce2654e
j-crowdplat <jlodha@crowdplat.com>
2025-04-04 19:01:01 -0700

    auth blueprint added

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS  # Import CORS
+from routes.auth import auth_bp
+# from routes.policies import policy_bp
+# from routes.masking import masking_bp
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Enable CORS for the entire app
+CORS(app)
+
+# Initialize JWT
+jwt = JWTManager(app)
+
+# Register the Blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
+# app.register_blueprint(policy_bp, url_prefix="/policy")
+# app.register_blueprint(masking_bp, url_prefix="/masking")
+
+@app.route('/')
+def index():
+    return "Welcome to the secure alley backend"
+
+if __name__ == '__main__':
+    app.run(debug=True)
